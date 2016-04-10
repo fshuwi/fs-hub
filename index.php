dee1bd0ec6784840c539796071c23d50296dfab2
Marc Kohaupt <marc.kohaupt@gmail.com>
2016-04-10 21:29:20 +0200

    add initializr.com stuff

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 <?php
-$host = "http://huwi.rocks";
-
 $users_titles = array();
 $users_titles["1_alle"] = "Für alle Menschen";
 $users_titles["2_students"] = "Für Studierende der Huwi";
 $users_titles["3_fachschaft"] = "Für Menschen der Fachschaft Huwi";
 $users_titles["4_admin"] = "Adminscheiß";
-
-
-
-#$services = array_map('str_getcsv', file('services.csv'));
-
 
 $csv = array_map('str_getcsv', file("services.csv"));
 array_walk($csv, function(&$a) use ($csv) {
@@ -20,37 +13,8 @@ array_shift($csv); # remove column header
 
 $services = $csv;
 
-#var_dump($csv);
-
-/*
-$services = array();
-$services[] = array('title' => 'Liberate PDF',		'url' => "$host:28080/LiberatePDF/uploadPdf.jsp",	'users' => 'all',	'status' => 'working');
-
-$services[] = array('title' => 'LimeSurvey',		'url' => "$host/limesurvey",		'users' => 'huwi',	'status' => 'working');
-
-$services[] = array('title' => 'ownCloud',		'url' => "$host/owncloud",		'users' => 'fachschaft',	'status' => 'working');
-$services[] = array('title' => 'Etherpad',		'url' => "$host:9001",			'users' => 'fachschaft',	'status' => 'working');
-$services[] = array('title' => 'Etherpad List',		'url' => "$host:9001/list",		'users' => 'fachschaft',	'status' => 'working');
-$services[] = array('title' => 'Framadate',		'url' => "$host/framadate",		'users' => 'fachschaft',	'status' => 'working');
-$services[] = array('title' => 'Termine',		'url' => "$host/termine",		'users' => 'fachschaft',	'status' => 'todo');
-$services[] = array('title' => 'GitLab',		'url' => "$host/gitlab",		'users' => 'fachschaft',	'status' => 'broken');
-$services[] = array('title' => 'Roundcube',		'url' => "$host/roundcube",		'users' => 'fachschaft',	'status' => 'working');
-
-$services[] = array('title' => 'phpMyAdmin',		'url' => "$host/phpmyadmin",		'users' => 'admin',	'status' => 'working');
-$services[] = array('title' => 'phpLDAPadmin',		'url' => "$host/phpldapadmin",		'users' => 'admin',	'status' => 'working');
-$services[] = array('title' => 'icinga (Nagios)',	'url' => "$host/icingaweb2",		'users' => 'admin',	'status' => 'working');
-$services[] = array('title' => 'Etherpad Admin',	'url' => "$host:9001/admin",		'users' => 'admin',	'status' => 'working');
-$services[] = array('title' => 'WildFly Admin',		'url' => "$host:9990",			'users' => 'admin',	'status' => 'working');
-$services[] = array('title' => 'phpinfo',		'url' => "$host/admininfo/phpinfo.php",	'users' => 'admin',	'status' => 'working');
-$services[] = array('title' => 'Tomcat 7 manager',	'url' => "$host:8007/manager/html",	'users' => 'admin',     'status' => 'working');
-$services[] = array('title' => 'Tomcat 7 host-manager',	'url' => "$host:8007/host-manager/html",'users' => 'admin',     'status' => 'working');
-$services[] = array('title' => 'Tomcat 8 manager',	'url' => "$host:8008/manager/html",	'users' => 'admin',     'status' => 'working');
-$services[] = array('title' => 'Tomcat 8 host-manager',	'url' => "$host:8008/host-manager/html",'users' => 'admin',     'status' => 'working');
-$services[] = array('title' => 'Piwik',			'url' => "$host/piwik",			'users' => 'admin',     'status' => 'working');
-$services[] = array('title' => 'temporary public',	'url' => "$host/temporary-public",	'users' => 'admin',     'status' => 'working');
-*/
-
 ?>
+
 <html>
   <head>
     <title>Fachschaft Huwi Hub</title>
@@ -61,7 +25,6 @@ $services[] = array('title' => 'temporary public',	'url' => "$host/temporary-pub
   </head>
   <body>
   <img id="logo" src="kHuWi.svg" style="max-width: 10%;">
-
 
 <?php
 
